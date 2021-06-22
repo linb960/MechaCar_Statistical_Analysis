d2eac9082e3ae27647d3e429fd4d9dcd58bcc4df
LBerez <lberez@mac.com>
2021-06-22 12:57:16 -0500

    Adding Deliverable 2  code

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -9,3 +9,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))
 
+# Deliverable 2
+SuspensionCoil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+head(SuspensionCoil_table)
+
+total_summary <- SuspensionCoil_table %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+total_summary
+
+lot_summary <- SuspensionCoil_table %>% group_by(Manufacturing_Lot)  %>%  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sqrt(var(PSI)), .groups = 'keep')
+lot_summary
